0ba40326d33f4f265fde060f0dca72b849f4dae7
Tushar Chaudhary <tushar.chaudhary@tushar.chaudhary-LFG2WD0J59>
2024-11-08 09:46:35 +0530

    Changes

--- main.tf
+++ main.tf
@@ -5,6 +5,4 @@ module "cis_v1-2-0_policies" {
   policy_github_repository             = var.policy_github_repository
   tfe_organization                     = var.tfe_organization
   policy_set_workspace_names           = var.policy_set_workspace_names
-
-  # insert the 11 required variables here
 }
--- variables.tf
+++ variables.tf
@@ -1,23 +1,19 @@
 variable "tfe_organization" {
   description = "The TFE organization where Sentinel based policy sets will be created. These policies will run against all the workspaces present in the organization"
   type        = string
-  default = "<your-tfe-org>"
 }
 
 variable "policy_set_workspace_names" {
   description = "List of workspace names to scope the policy set to."
   type        = list(string)
-  default = ["target_workspace_1"]
 }
 
 variable "name" {
   description = "Common suffix/prefix prepended/appended to all the resources getting created with this module."
   type        = string
-  default     = "cis-1-2-0"
 }
 
 variable "policy_github_repository" {
   description = "The name of the GitHub repository where the policies reside. This name should not include the GitHub organization."
   type        = string
-  default = "policy-library-aws-cis-v1.2.0-terraform"
 }
